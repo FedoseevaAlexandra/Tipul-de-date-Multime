e5dfdf5d7d146e3653db015f0067d40d98d597b0
FedoseevaAlexandra <70970063+FedoseevaAlexandra@users.noreply.github.com>
2021-11-14 14:53:45 +0300

    Add files via upload

---
+++ pr_9_p_60.py
@@ -0,0 +1,18 @@
+A=set(input('dati elementele multimii A :'))
+
+B=set(input('dati elementele multimii B :'))
+x=0
+for i in A:
+    if ord(i) not in range(65,91):
+        x+=1
+
+for i in B:
+   if ord(i) not in range(65,91):
+        x+=1
+if x>0:
+    print('Mulţimile A şi B trebuie sa fie formate din n literele mari ale alfabetului latin.')
+if x==0:
+  print('intersecţia mulţimilor A şi B =',A.intersection(B))
+  print('reuniunea mulţimilor A şi B =',A.union(B))
+  print('diferenţa mulţimilor A şi B =',A.difference(B))
+  print('diferenţa mulţimilor B şi A =',B.difference(A))
